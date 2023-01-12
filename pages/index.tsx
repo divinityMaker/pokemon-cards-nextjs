5ef8aa044f3e95d7251323126af0167964591296
divinityMaker <truedmaker@gmail.com>
2023-01-12 18:50:00 -0300

    chore: add temp button to navigate between cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return (
     <div className={styles.main}>
-
+      <Link href={'/giratina'}>Giratina</Link>
+      <br/>
+      <Link href={'/ninetales'}>Ninetales</Link>
     </div>
   )
 }
